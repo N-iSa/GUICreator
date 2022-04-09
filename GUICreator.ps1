b00817202f62602139433be13d919b7f09510e4e
l3ye <derivativeofleye@gmail.com>
2022-04-09 20:37:02 +0200

    Change Name and Text
    It is now possible to change the Name and the Text of GUI-Controls

--- GUICreator.ps1
+++ GUICreator.ps1
@@ -3,7 +3,7 @@
 [void][Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
 #region
-$objAusgewaehlt = $null
+$objSelected = $null
 $bMouseDown = $false
 #endregion
 
@@ -83,6 +83,55 @@ $grpGUIElementSettings = New-Object System.Windows.Forms.GroupBox
 $grpGUIElementSettings.Text = "GUI Settings"
 $grpGUIElementSettings.Dock = [System.Windows.Forms.DockStyle]::Right
 $grpGUIElementSettings.Width = 400
+
+<#
+$ = New-Object System.Windows.Forms
+$.Location = New-Object System.Drawing.Size()
+$.Text = ""
+#>
+
+# Setting: Name
+$lblSettingName = New-Object System.Windows.Forms.Label
+$lblSettingName.Location = New-Object System.Drawing.Size(10, 20)
+$lblSettingName.Size = New-Object System.Drawing.Size((40), ($lblSettingName.Height))
+$lblSettingName.Text = "Name:"
+
+$txtSettingName = New-Object System.Windows.Forms.TextBox
+$txtSettingName.Location = New-Object System.Drawing.Size(($lblSettingName.Location.X + $lblSettingName.Width + 5), ($lblSettingName.Location.Y - 2))
+$txtSettingName.Text = ""
+
+# Setting: Text
+$lblSettingText = New-Object System.Windows.Forms.Label
+$lblSettingText.Location = New-Object System.Drawing.Size(($lblSettingName.Location.X), ($txtSettingName.Location.Y + $lblSettingText.Height + 5))
+$lblSettingText.Size = New-Object System.Drawing.Size((40), ($lblSettingName.Height))
+$lblSettingText.Text = "Text:"
+
+$txtSettingText = New-Object System.Windows.Forms.TextBox
+$txtSettingText.Location = New-Object System.Drawing.Size(($lblSettingText.Location.X + $lblSettingText.Width + 5), ($lblSettingText.Location.Y - 2))
+$txtSettingText.Text = ""
+<#
+# Setting: Enabled
+$lblSettingEnabled = New-Object System.Windows.Forms.Label
+$lblSettingEnabled.Location = New-Object System.Drawing.Size()
+$lblSettingEnabled.Text = ""
+
+$txtSettingEnabled = New-Object System.Windows.Forms.TextBox
+$txtSettingEnabled.Location = New-Object System.Drawing.Size()
+$txtSettingEnabled.Text = ""
+
+# Setting: Location
+$lblSettingLocation = New-Object System.Windows.Forms.Label
+$lblSettingLocation.Location = New-Object System.Drawing.Size()
+$lblSettingLocation.Text = ""
+
+$numSettingLocationX = New-Object System.Windows.Forms.NumericUpDown
+$numSettingLocationX.Location = New-Object System.Drawing.Size()
+$numSettingLocationX.Text = ""
+
+$numSettingLocationY = New-Object System.Windows.Forms.NumericUpDown
+$numSettingLocationY.Location = New-Object System.Drawing.Size()
+$numSettingLocationY.Text = ""
+#>
 #endregion
 
 
@@ -110,16 +159,28 @@ $grpGUIElements.Controls.Add($btnAddGUIElementCombobox)
 $grpGUIElements.Controls.Add($btnAddGUIElementListbox)
 $grpGUIElements.Controls.Add($btnAddGUIElementTextbox)
 $grpGUIElements.Controls.Add($btnAddGUIElementButton)
+
+$grpGUIElementSettings.Controls.Add($lblSettingName)
+$grpGUIElementSettings.Controls.Add($txtSettingName)
+$grpGUIElementSettings.Controls.Add($lblSettingText)
+$grpGUIElementSettings.Controls.Add($txtSettingText)
 #endregion
 #endregion
 
 #region Events and Functions
 # Function
-function UpdateControlValues()
+function refreshElementSettings
 {
+    $Script:txtSettingName.Text = $Script:objSelected.Name
+    $Script:txtSettingText.Text = $Script:objSelected.Text
+}
+function clearElementSettings
+{
+    $Script:txtSettingName.Text = [String]::Empty
+    $Script:txtSettingText.Text = [String]::Empty
 }
 
-function addNewControl()
+function addNewControl
 {
     param(
         [Type] $type
@@ -135,8 +196,9 @@ function addNewControl()
         }
         else
         {
-            $Script:objAusgewaehlt = $sender
+            $Script:objSelected = $sender
             $Script:bMouseDown = $true
+            refreshElementSetting
         }
     })
     $obj.Add_MouseUp({
@@ -172,9 +234,24 @@ $btnAddGUIElementCheckbox.Add_Click({
     addNewControl System.Windows.Forms.Checkbox
 })
 
+$txtSettingName.Add_TextChanged({
+    if($Script:objSelected -ne $null)
+    {
+        $Script:objSelected.Name = $Script:txtSettingName.Text
+    }
+})
+
+$txtSettingText.Add_TextChanged({
+    if($Script:objSelected -ne $null)
+    {
+        $Script:objSelected.Text = $Script:txtSettingText.Text
+    }
+})
+
 $frmDesign.Add_LocationChanged({
     $frmDesign.Location = New-Object System.Drawing.Size(10, 20) 
-}) 
+})
+
 #endregion
 
 
